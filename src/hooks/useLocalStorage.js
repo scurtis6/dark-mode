6b77b3e443e8a61e2fb2108b8a6b134f6a8803e7
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-15 15:14:39 -0500

    returned storedvalue from hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,1 +1,14 @@
-export const useLocalStorage = (key, initialValue) ={}
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    // to retrieve an item from localStorage, call localStorage.getItem('someItem')
+    // if that item doesn't exist, it will return undefined
+    const [storedValue, setStoredValue] = useState(() => {
+        // get from local storage by key
+        const item = window.localStorage.getItem(key);
+        // parse and return stored json or, if undefined, return initialValue
+        return item ? JSON.parse(item) : initialValue;
+    });
+    return [storedValue];
+}
+
