2d30f5af3a47f01959c0fc5cb3b54efffa93c0d8
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-15 15:38:19 -0500

    add and remove class to the body

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,14 @@
 import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react';
 
 export const useDarkMode = () => {
     const [values, setValues] = useLocalStorage(key, initialValue);
-}
+
+    useEffect(() => {
+        if (values === true){
+            document.body.classList.add('dark-mode')
+        } else {
+            document.body.classList.remove('dark-mode')
+        }
+    });
+};
